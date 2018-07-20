abd8457d9344f09dc83f29d7968b7b971b463549
ganeshdipdumbare <ubuntu@ip-172-31-17-100.us-east-2.compute.internal>
2018-07-20 04:15:07 +0000

    Added new changes

--- MetaCoinNew/truffle.js
+++ MetaCoinNew/truffle.js
@@ -1,35 +1,13 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-require('dotenv').config();
-//const privKey = "bb951f4ab9c3665c69a187a3bf344b6b5ec0f9cc782de96132312011f92dec2b"; // raw private key
-//console.log(process.env.PRIVATE_KEY);
-var prov = new HDWalletProvider(process.env.MNEMONICS, "https://rinkeby.infura.io/PGeaCCgYpthSugPncips",0);
+const MNEMONICS=""remain drip sunset throw harbor security off capital mesh clump coach inner""
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-solc: {
-  optimizer: {
-      enabled: true,
-      runs: 200
-    }
-},
 networks: {
-  mainnet:{
-    provider: new HDWalletProvider(process.env.MNEMONICS, "https://mainnet.infura.io/PGeaCCgYpthSugPncips",0),
-    network_id: '1'
-  },
-  ropsten: {
-    provider: new HDWalletProvider(process.env.MNEMONICS, "https://ropsten.infura.io/PGeaCCgYpthSugPncips",0),
-    network_id: '3'
-  },
   rinkeby: {
-    provider: new HDWalletProvider(process.env.MNEMONICS, "https://rinkeby.infura.io/PGeaCCgYpthSugPncips",0),
+    provider: new HDWalletProvider(MNEMONICS, "https://rinkeby.infura.io/PGeaCCgYpthSugPncips",0),
     network_id: '4'
-  },
-  development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
-	}
+  }
 }
 };
